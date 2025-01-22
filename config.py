cfba6b01d16344a68335f2c930231779c44abdee
feeri-irwn <erif45810@gmail.com>
2025-01-23 00:14:27 +0700

    UAS

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ CORS(app,resources={r"/api/*": {"origins": "*"}},supports_credentials=True)
 #CORS(app, origins=["http://localhost:5173"])
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://feri-sql:1234@localhost:3306/uas-sql'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://uassql_doesvapor:2561f8d0054c0561451c32aa271481d17d7675cc@x7v16.h.filess.io:3306/uassql_doesvapor'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://uassql_easiermad:07aaad51578161deed4761c090d8722c793abd71@u4vzz.h.filess.io:3307/uassql_easiermad'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '1234'
 
